94485bfe3e1598df9351b42011f78343a507211b
Sreehari jayaraj <66205494+sreehari2003@users.noreply.github.com>
2022-01-21 18:00:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/dataRoutes");
+app.get("/",(req,res)=>{
+ res.send("<h1>hello world</h1>");
+})
 app.use(express.json());
 app.use("/api", userRoutes);
 
