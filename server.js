739a9c4102a0d7b79862e05e4ebebac4b3c460cb
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-01-21 23:49:03 +0530

    cors policy bug

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 const morgan = require("morgan");
 const app = require("./app");
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 dotenv.config({ path: "./config.env" });
 const port = 4000;
 
