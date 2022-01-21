6cf91f26816db91354711eaa798621805b7f278d
Sreehari jayaraj <66205494+sreehari2003@users.noreply.github.com>
2022-01-22 00:21:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 const userRoutes = require("./routes/dataRoutes");
 app.get("/",(req,res)=>{
  res.send("<h1>hello world</h1>");
