b5c265e5f58fd13402666f7302ed69d35302cbb2
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-03-24 13:58:10 +0530

    added cors origins

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import express,{Request,Response} from 'express';
 const app = express();
 import morgan from "morgan";
+import cors from "cors";
+
+
+const corsOptions = {
+  origin: ["https://typeintern.netlify.app/","http://localhost:3000/"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 
 if (process.env.NODE_ENV !== "production") {
